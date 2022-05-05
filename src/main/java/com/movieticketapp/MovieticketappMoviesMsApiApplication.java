76ff999157aff1ffcc2c46005dd44c40e9601ee2
Murugan2207 <vickyvenkat2207@gmail.com>
2022-05-05 18:11:34 +0530

    added src

---
+++ src/main/java/com/movieticketapp/MovieticketappMoviesMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.movieticketapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieticketappMoviesMsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieticketappMoviesMsApiApplication.class, args);
+	}
+
+}
